a9252d8e01f01d00df0980325c873e2c6f17ba12
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-26 00:30:19 +0500

    login and signup is wornig fine!

--- src/index.js
+++ src/index.js
@@ -2,39 +2,53 @@ const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 const fs = require("fs");
 const path = require("path");
+const { getUserId } = require("./utils");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    users: async (parent, args, context) => {
-      return context.prisma.user.findMany();
-    },
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      const newUser = context.prisma.user.create({
-        data: {
-          name: args.name,
-          email: args.email,
-          password: args.password,
-          surname: args.surname,
-          profession: args.profession,
-          onvacation: args.onvacation,
-        },
-      });
-
-      return newUser;
-    },
-  },
+  Query,
+  Mutation,
+  User,
 };
+// const resolvers = {
+//   Query: {
+//     info: () => `This is the API of a Hackernews Clone`,
+//     users: async (parent, args, context) => {
+//       return context.prisma.user.findMany();
+//     },
+//   },
+//   Mutation: {
+//     post: (parent, args, context) => {
+//       const newUser = context.prisma.user.create({
+//         data: {
+//           name: args.name,
+//           email: args.email,
+//           password: args.password,
+//           surname: args.surname,
+//           profession: args.profession,
+//           onvacation: args.onvacation,
+//         },
+//       });
+
+//       return newUser;
+//     },
+//   },
+// };
 
 // 3
 const prisma = new PrismaClient();
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,70 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("../utils");
+
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10);
+
+  const user = await context.prisma.user.create({
+    data: {
+      email: args.email,
+      name: args.name,
+      surname: args.surname,
+      profession: args.profession,
+      onvacation: args.onvacation,
+      password: password,
+    },
+  });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+async function login(parent, args, context, info) {
+  // 1
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  });
+  if (!user) {
+    throw new Error("No such user found");
+  }
+
+  // 2
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  // 3
+  return {
+    token,
+    user,
+  };
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+};
+
+async function post(parent, args, context, info) {
+  const { userId } = context;
+
+  return await context.prisma.user.create({
+    data: {
+      name: args.name,
+      surname: args.surname,
+      email: args.email,
+      password: args.password,
+      profession: args.profession,
+      onvacation: args.onvacation,
+    },
+  });
+}
